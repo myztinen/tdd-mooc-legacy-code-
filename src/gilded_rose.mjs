1c1f02e125877f55227808bd250ab63f7ea7ffb0
myztinen <myztinen@gmail.com>
2024-04-16 18:29:20 +0300

    Each item type has now its own update function

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -14,41 +14,56 @@ export class Shop {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if(this.items[i].name == "Sulfuras, Hand of Ragnaros" ) {
-        
+        this.updatQualityForSulfuras(i);
       } else if (this.items[i].name == "Aged Brie") {
-        if (this.items[i].sellIn < 1) {
-          this.increaseQuality(i, 2);
-        }  else this.increaseQuality(i);
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.updateAgeBrieQuality(i);
       } else if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-        if(this.items[i].sellIn < 1) {
-          this.items[i].quality = this.items[i].quality - this.items[i].quality;
-        } 
-        if (this.items[i].sellIn > 0) {
-          this.increaseQuality(i);
-          if (this.items[i].sellIn < 11) {
-            this.increaseQuality(i);
-          }
-          if (this.items[i].sellIn < 6) {
-            this.increaseQuality(i);
-          }
-        }
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.updateBackstagePassQuality(i);
       } else {
-        if ( this.items[i].quality > 0) {
-          this.reduceQuality(i);
-
-          if (this.items[i].sellIn <= 0) {
-            this.reduceQuality(i);
-          }
-        }
-
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.updateQualityForNormalItems(i);
       }
     }
     return this.items;
   }
+
+  updatQualityForSulfuras(i) {
+    
+  }
   
+  updateAgeBrieQuality(i) {
+    if (this.items[i].sellIn < 1) {
+      this.increaseQuality(i, 2);
+    } else this.increaseQuality(i);
+    this.items[i].sellIn--;// = this.items[i].sellIn - 1;
+  }
+
+  updateBackstagePassQuality(i) {
+    if (this.items[i].sellIn < 1) {
+      this.items[i].quality = this.items[i].quality - this.items[i].quality;
+    }
+    if (this.items[i].sellIn > 0) {
+      this.increaseQuality(i);
+      if (this.items[i].sellIn < 11) {
+        this.increaseQuality(i);
+      }
+      if (this.items[i].sellIn < 6) {
+        this.increaseQuality(i);
+      }
+    }
+    this.items[i].sellIn--;
+  }
+
+  updateQualityForNormalItems(i) {
+    if (this.items[i].quality > 0) {
+      this.reduceQuality(i);
+
+      if (this.items[i].sellIn <= 0) {
+        this.reduceQuality(i);
+      }
+    }
+    this.items[i].sellIn--;
+  }
+
   reduceQuality(itemIndex, amount=1) {
     this.items[itemIndex].quality = Math.max(0, this.items[itemIndex].quality - amount);
   }
